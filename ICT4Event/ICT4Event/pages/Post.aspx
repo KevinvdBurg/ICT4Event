7f7a885ad9e4c2f7de7d9bccf4bd23c6550d452f
bryzerker <bryanweever9@Hotmail.com>
2015-06-02 15:24:17 +0200

    Posts opnieuw toegevoegd, contols toegevoegd

---
+++ ICT4Event/ICT4Event/pages/Post.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/main.master" AutoEventWireup="true" CodeBehind="Post.aspx.cs" Inherits="ICT4Event.pages.Post" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:ListBox ID="lbCategorie" runat="server" AutoPostBack="True" Height="200px" OnSelectedIndexChanged="lbCategorie_SelectedIndexChanged" Width="100px"></asp:ListBox>
+    <asp:ListBox ID="lbItems" runat="server" AutoPostBack="True" Height="200px" OnSelectedIndexChanged="lbItems_SelectedIndexChanged" Width="100px"></asp:ListBox>
+   
+    <asp:Image ID="Image1" runat="server" />
+   
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+     <asp:TextBox ID="tbInhoud" runat="server" Height="200px" ReadOnly="True" TextMode="MultiLine" Visible="False" Width="400px"></asp:TextBox>
+</asp:Content>
---
+++ ICT4Event/ICT4Event/pages/Post.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ICT4Event.pages
+{
+    public partial class Post : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Je kan eerst kiezen uit berichten en bestanden
+            if (!IsPostBack)
+            {
+                lbCategorie.Items.Add("bericht");
+                lbCategorie.Items.Add("bestand");
+            }
+            
+        }
+
+        protected void lbCategorie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Eerst lb clearen
+            lbItems.Items.Clear();
+
+            //refill lbCategorie indien van toepassing
+
+            //Als er op berichten word geklikt laat hij gewoon berichten zien
+            //Als er op bestand word geklikt worden er categorieën getoond, of bestanden zonder categorie
+            lbItems.Items.Add("Bericht1");
+            lbItems.Items.Add("Bericht2");
+            
+        }
+
+        protected void lbItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            tbInhoud.Text = "Inhoud van bericht1";
+            tbInhoud.Visible = true;
+        }
+    }
+}
