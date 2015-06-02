723642097994829ac74568139e526afabedbfbdb
BartMemelink <b.memelink@student.fontys.nl>
2015-06-02 15:41:52 +0200

    Inhoud pagina

---
+++ ICT4Event/ICT4Event/pages/Reserve.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ICT4Event.pages
+{
+    
+    public partial class Reserve : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+            }
+        }
+
+        protected void btnReserve_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
