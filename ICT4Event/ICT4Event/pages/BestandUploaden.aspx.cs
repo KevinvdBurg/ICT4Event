bc0eadce3c198e0ae3d39f709d480b77f119d3c4
Bastheking11 <Basvanweerdenburg11@hotmail.com>
2015-06-03 12:46:30 +0200

    Bestand uploaden

---
+++ ICT4Event/ICT4Event/pages/BestandUploaden.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ICT4Event
+{
+    using System.IO;
+
+    public partial class BestandUploaden : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click1(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                try
+                {
+                    string filename = Path.GetFileName(FileUploadControl.FileName);
+                    FileUploadControl.SaveAs(Server.MapPath("~/") + filename);
+                    StatusLabel.Text = "Upload status: File uploaded!";
+                }
+                catch (Exception ex)
+                {
+                    StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
+        }
+    }
+}
