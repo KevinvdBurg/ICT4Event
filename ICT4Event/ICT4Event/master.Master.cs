aad839394db8d2f48027f466f0a97f9f50d3c1b3
KevinvdBurg <vanderburg.kevin@gmail.com>
2015-06-12 14:58:42 +0200

    fixed namespace

--- ICT4Event/ICT4Event/master.Master.cs
+++ ICT4Event/ICT4Event/master.Master.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace lastfm
+namespace ICT4Event
 {
     public partial class master : System.Web.UI.MasterPage
     {
