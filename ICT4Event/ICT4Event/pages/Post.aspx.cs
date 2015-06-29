026c8e879bb9904f73f9dc4ad8d9f6a183cf6b3e
bryzerker <bryanweever9@Hotmail.com>
2015-06-29 13:23:08 +0200

    .

--- ICT4Event/ICT4Event/pages/Post.aspx.cs
+++ ICT4Event/ICT4Event/pages/Post.aspx.cs
@@ -6,6 +6,9 @@
 //   The post.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
+
+using System.Net;
+
 namespace ICT4Event.pages
 {
     using System;
@@ -283,6 +286,25 @@ namespace ICT4Event.pages
         /// </param>
         protected void btnDownload_Click(object sender, EventArgs e)
         {
+            try
+            {
+                string type = lbItems.SelectedValue.Substring(lbItems.SelectedValue.LastIndexOf('-') + 1);
+                /*string strURL = lbItems.SelectedValue;
+                WebClient req = new WebClient();
+                HttpResponse response = HttpContext.Current.Response;
+                response.Clear();
+                response.ClearContent();
+                response.ClearHeaders();
+                response.Buffer = true;
+                response.AddHeader("Content-Disposition", "attachment;filename=\"" + Server.MapPath(strURL) + "\"");
+                //byte[] data = req.DownloadData(Server.MapPath(strURL));
+                req.DownloadFile(Server.MapPath("~/Uploads/"), strURL);
+                //response.BinaryWrite(data);
+                response.End();*/
+            }
+            catch (Exception ex)
+            {
+            }
             //this.ftptj.DownloadFtpFile(
             //    Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
             //    this.lbItems.SelectedValue,
