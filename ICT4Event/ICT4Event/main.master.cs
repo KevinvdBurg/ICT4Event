40168649af8741f3b13730bd30739f95242e46cd
KevinvdBurg <vanderburg.kevin@gmail.com>
2015-06-30 13:26:59 +0200

    ReAdded AD check

--- ICT4Event/ICT4Event/main.master.cs
+++ ICT4Event/ICT4Event/main.master.cs
@@ -41,56 +41,56 @@ namespace ICT4Event
             //Beta testing 
 
             //---------------------------
-            this.Session[MyKeys.KeyAccountId] = 9;
-            this.Session[MyKeys.KeyEmail] = "kevin@ict4events.bb";
-            this.Session[MyKeys.KeyUsername] = "kevin";
+            //this.Session[MyKeys.KeyAccountId] = 9;
+            //this.Session[MyKeys.KeyEmail] = "kevin@ict4events.bb";
+            //this.Session[MyKeys.KeyUsername] = "kevin";
             //---------------------------
 
             string value = Session[MyKeys.KeyAccountId] + string.Empty;
 
-            //if (string.IsNullOrEmpty(value))
-            //{
-            //    string urlSting = Regex.Replace(HttpContext.Current.Request.Url.AbsolutePath, @"\s+", string.Empty);
+            if (string.IsNullOrEmpty(value))
+            {
+                string urlSting = Regex.Replace(HttpContext.Current.Request.Url.AbsolutePath, @"\s+", string.Empty);
 
-            //    if (urlSting == "/index.aspx" || urlSting == "/pages/registreren.aspx")
-            //    {
-            //        Response.Write("Gelijk");
-            //    }
-            //    else
-            //    {
-            //        Response.Redirect("/index.aspx");
-            //    }
-            //}
-            //else
-            //{
-            //    List<string> groupsList =
-            //        this.administration.GetAccountGroups(Convert.ToString(Session[MyKeys.KeyUsername]));
-            //    bool admin = false;
-            //    foreach (string group in groupsList)
-            //    {
-            //        if (group == "Web-Admin")
-            //        {
-            //            admin = true;
-            //        }
-            //    }
+                if (urlSting == "/index.aspx" || urlSting == "/pages/registreren.aspx")
+                {
+                    Response.Write("Gelijk");
+                }
+                else
+                {
+                    Response.Redirect("/index.aspx");
+                }
+            }
+            else
+            {
+                List<string> groupsList =
+                    this.administration.GetAccountGroups(Convert.ToString(Session[MyKeys.KeyUsername]));
+                bool admin = false;
+                foreach (string group in groupsList)
+                {
+                    if (group == "Web-Admin")
+                    {
+                        admin = true;
+                    }
+                }
 
-            //    if (admin)
-            //    {
-            //        //MessageBox.Show(this.Page, "Web-Admin");
-            //        this.admin_opties.Text = "<li class=\"sub-flex-item\"><a href=\"/pages/Toegang.aspx\">Admin</a></li'>";
-            //    }
-            //    else
-            //    {
-            //        // MessageBox.Show(this.Page, "Web-Gebruiker");
-            //        //this.Page.Show(
-            //        //    this.Session[MyKeys.KeyUsername] + " - " + this.Session[MyKeys.KeyEmail] + " - "
-            //        //    + this.Session[MyKeys.KeyAccountId]);
+                if (admin)
+                {
+                    //MessageBox.Show(this.Page, "Web-Admin");
+                    this.admin_opties.Text = "<li class=\"sub-flex-item\"><a href=\"/pages/Toegang.aspx\">Admin</a></li'>";
+                }
+                else
+                {
+                    // MessageBox.Show(this.Page, "Web-Gebruiker");
+                    //this.Page.Show(
+                    //    this.Session[MyKeys.KeyUsername] + " - " + this.Session[MyKeys.KeyEmail] + " - "
+                    //    + this.Session[MyKeys.KeyAccountId]);
 
-            //        // Response.Redirect("to user page");
-            //    }
+                    // Response.Redirect("to user page");
+                }
 
-            //    this.btn_inuitlog.Text = "Uitloggen";
-            //}
+                this.btn_inuitlog.Text = "Uitloggen";
+            }
         }
 
         /// <summary>
